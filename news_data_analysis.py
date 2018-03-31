20fefbde3738088586a3c5679f743493d0a504f6
Ricardo Rivera <rick_rivera662@yahoo.com>
2018-03-30 22:19:52 -0700

    Update to prevent SQL injection.

--- news_data_analysis.py
+++ news_data_analysis.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 import psycopg2
+from psycopg2 import sql
 
 
-def get_top_articles(cur, order, limit):
+def get_top_articles(cur, limit):
     """Fetches the top articles.
 
     Fetches the number of top articles in the specified
@@ -10,20 +11,20 @@ def get_top_articles(cur, order, limit):
 
     Args:
         cur(obj): The cursor to execute the query.
-        order(str): The order to view the rows in.
         limit(int): The number of rows to view.
 
     Return:
         True if success, False otherwise.
     """
+    data = (limit, )
     query = '''SELECT articles.title, COUNT(*) as views
             FROM log, articles
-            WHERE log.path LIKE '%'||articles.slug AND
+            WHERE log.path = '/article/'||articles.slug AND
             log.method = 'GET'
             GROUP BY articles.title
-            ORDER BY views {}
-            LIMIT {}'''.format(order, limit)
-    rows = get_data(cur, query)
+            ORDER BY views DESC
+            LIMIT %s'''
+    rows = get_data(cur, query, data)
 
     # Write data to txt file.
     if rows is not None:
@@ -37,24 +38,24 @@ def get_top_articles(cur, order, limit):
         return False
 
 
-def get_top_authors(cur, order):
+def get_top_authors(cur):
     """Fetches the top authors.
 
     Args:
         cur(obj): The cursor to execute the query.
-        order(str): The order to view the rows in.
 
     Return:
         True if success, False otherwise.
     """
+    data = ()
     query = '''SELECT authors.name, COUNT(*) as views
             FROM authors, articles, log
             WHERE authors.id = articles.author AND
-            log.path LIKE '%'||articles.slug AND
+            log.path = '/article/'||articles.slug AND
             log.method = 'GET'
             GROUP BY authors.name
-            ORDER BY views {}'''.format(order)
-    rows = get_data(cur, query)
+            ORDER BY COUNT(*) DESC'''
+    rows = get_data(cur, query, data)
 
     # Write data to txt file.
     if rows is not None:
@@ -81,15 +82,16 @@ def get_error_days(cur, error_percent):
     Return:
         True if success, False otherwise.
     """
+    data = (error_percent, )
     query = '''SELECT to_char(log_errors.date, 'Mon DD YYYY'),
             round((log_errors.errors * 100
             / log_requests.total::numeric), 2) as percent
             FROM log_errors, log_requests
             WHERE log_errors.date = log_requests.date AND
             log_errors.errors * 100
-            / log_requests.total::numeric > {}
-            ORDER BY log_errors.date'''.format(error_percent)
-    rows = get_data(cur, query)
+            / log_requests.total::numeric > %s
+            ORDER BY log_errors.date'''
+    rows = get_data(cur, query, data)
 
     # Write data to txt file.
     if rows is not None:
@@ -103,20 +105,22 @@ def get_error_days(cur, error_percent):
         return False
 
 
-def get_data(cur, query):
+def get_data(cur, query, data):
     """Fetches the data specified in the query.
 
     Args:
         cur(obj): The cursor to execute the query.
         query(str): The query to execute.
+        data(tuple): The values to insert into the query.
 
     Return:
         The data or None if there is an error.
     """
     try:
-        cur.execute(query)
+        cur.execute(query, data)
         return cur.fetchall()
-    except psycopg2.Error:
+    except psycopg2.Error as e:
+        print(e)
         cur.connection.rollback()
         return None
 
@@ -146,12 +150,12 @@ def main():
     if conn is not None:
         cur = conn.cursor()
         # Create top articles report.
-        if get_top_articles(cur, "DESC", 3):
+        if get_top_articles(cur, 3):
             print("Successful creating top articles report.")
         else:
             print("Error creating top articles report.")
         # Create top authors report.
-        if get_top_authors(cur, "DESC"):
+        if get_top_authors(cur):
             print("Successful creating top authors report.")
         else:
             print("Error creating top authors report.")
@@ -163,4 +167,5 @@ def main():
 
         conn.close()
 
-main()
+if __name__ == '__main__':
+    main()
